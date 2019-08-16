624fc8551e0fa3bf978400b0056435e60e858434
Learner <vivek.gurukul1982@gmail.com>
2019-08-16 08:05:23 -0700

    Create product-sales-analysis-ii.sql

---
+++ product-sales-analysis-ii.sql
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/product-sales-analysis-ii/
+# Write your MySQL query statement below
+select product_id, SUM(quantity) as total_quantity
+from Sales
+group by product_id;
