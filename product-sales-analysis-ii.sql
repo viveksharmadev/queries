d8c6723f6f86e4f325ed2811b48ae0b842f41620
Learner <vivek.gurukul1982@gmail.com>
2019-08-16 08:06:01 -0700

    Update product-sales-analysis-ii.sql

--- product-sales-analysis-ii.sql
+++ product-sales-analysis-ii.sql
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/product-sales-analysis-ii/
+-- https://leetcode.com/problems/product-sales-analysis-ii/
 # Write your MySQL query statement below
 select product_id, SUM(quantity) as total_quantity
 from Sales
